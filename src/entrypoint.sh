38b24342e5428ce4e6a87eb45c35e6d6a52456f7
homeserver <mayhome35@gmail.com>
2025-04-09 18:15:21 +0530

    Update entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,16 +1,6 @@
 #!/bin/sh
-
-PUID=${PUID:-1001}
-PGID=${PGID:-1001}
-
-if ! getent group customgroup >/dev/null 2>&1; then
-    addgroup -g "$PGID" rclonemanager
-fi
-
-if ! id -u rclonemanager >/dev/null 2>&1; then
-    adduser -u "$PUID" -G rclonemanager -D -s /bin/sh rclonemanager
-fi
-
-chown -R rclonemanager:rclonemanager /data
-
-exec su-exec rclonemanager "/usr/local/bin/rclone-manager"
+echo "Entrypoint script running as user: $(whoami) (UID: $(id -u))"
+echo "Executing rclone-manager..."
+# Execute the main application directly.
+# The container's USER instruction ensures this runs as the correct user.
+exec "/usr/local/bin/rclone-manager"
